7bcc71786a1ed05cf667f97cb27177ba32e79c3c
csnexuverse-hub <csnexuverse@gmail.com>
2026-04-29 15:55:31 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,12 +3,17 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'books.google.com' },
-      { protocol: 'http', hostname: 'covers.openlibrary.org' },
+      {
+        protocol: 'https',
+        hostname: 'books.google.com',
+      },
+      {
+        protocol: 'http',
+        hostname: 'covers.openlibrary.org',
+      },
     ],
     unoptimized: true,
   },
-  output: 'standalone',
 }
 
 module.exports = nextConfig
